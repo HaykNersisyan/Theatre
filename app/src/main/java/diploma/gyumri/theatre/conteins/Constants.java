1c32d644594cf0654332ff73636c45ebf8dc4598
Hayk Nersisyan <nersisyan90000@gmail.com>
2017-08-08 16:10:56 +0400

    create ContactUsFragment (demo)

--- app/src/main/java/diploma/gyumri/theatre/conteins/Constants.java
+++ app/src/main/java/diploma/gyumri/theatre/conteins/Constants.java
@@ -12,4 +12,5 @@ public class Constants {
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     public static final Pattern VALID_PASSWORD_REGEX =
             Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{8,12}$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_PHONE_REGEX = Pattern.compile("^[0-9\\-]{8,13}$\n");
 }
