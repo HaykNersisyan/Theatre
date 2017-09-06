92c043182a4fce14f0c939a510cc304e4ed0f046
Hayk Nersisyan <nersisyan90000@gmail.com>
2017-09-06 18:44:15 +0400

    login and registration

--- app/src/main/java/diploma/gyumri/theatre/model/User.java
+++ app/src/main/java/diploma/gyumri/theatre/model/User.java
@@ -1,12 +1,9 @@
 package diploma.gyumri.theatre.model;
 
-import android.widget.EditText;
+import android.util.Log;
 
-import butterknife.BindView;
-import diploma.gyumri.theatre.R;
 import io.realm.RealmObject;
 import lombok.Data;
-import lombok.ToString;
 
 /**
  * Created by Hayk on 03.08.2017.
@@ -18,7 +15,7 @@ public class User extends RealmObject {
     private String password;
     private String confirmPassword;
     private String name;
-    private String surName;
+    private String surname;
     private String phone;
     private String email;
     private String token;
@@ -28,14 +25,15 @@ public class User extends RealmObject {
     }
 
     public User(String login, String password, String confirmPassword,
-                String name, String surName, String phone, String email) {
+                String name, String surname, String phone, String email) {
         this.login = login;
         this.password = password;
         this.confirmPassword = confirmPassword;
         this.name = name;
-        this.surName = surName;
+        this.surname = surname;
         this.phone = phone;
         this.email = email;
+        Log.i("TAGUHI", "User: " + this.surname);
     }
 
     @Override
@@ -43,7 +41,7 @@ public class User extends RealmObject {
         return "User{" +
                 "login='" + login + '\'' +
                 ", name='" + name + '\'' +
-                ", surName='" + surName + '\'' +
+                ", surname='" + surname + '\'' +
                 ", phone='" + phone + '\'' +
                 ", email='" + email + '\'' +
                 ", token='" + token + '\'' +
--- app/src/main/java/diploma/gyumri/theatre/net/APIService.java
+++ app/src/main/java/diploma/gyumri/theatre/net/APIService.java
@@ -6,6 +6,7 @@ import diploma.gyumri.theatre.constants.Constants;
 import diploma.gyumri.theatre.data.dto.EventsDTO;
 import diploma.gyumri.theatre.data.dto.UserDTO;
 import diploma.gyumri.theatre.data.dto.UserResponseDTO;
+import diploma.gyumri.theatre.model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -19,7 +20,9 @@ public interface APIService {
     @GET(Constants.BASE_URL + "events/list")
     Call<EventsDTO> getEvents();
 
-    //    @Headers({"Content-type:aplication/json"})
     @POST(Constants.BASE_URL + "login")
     Call<UserDTO> login(@Body UserDTO user);
+
+    @POST(Constants.BASE_URL + "register")
+    Call<User> register(@Body User user);
 }
